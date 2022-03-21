a84e0b3cc7d2556edc3d98efd4f79162262fcd9a
Trần Nguyên <nguyentran0113@gmail.com>
2022-03-21 21:58:31 +0700

    update continuously

---
+++ src/database/fixDatabase.js
@@ -0,0 +1,41 @@
+import db from "./index";
+
+const intervals = ["1m"];
+// const intervals = ["1m", "5m", "10m"];
+
+export default async () => {
+  const coins = await db("binance_trading_pairs")
+    .join("coins", "binance_trading_pairs.baseAsset", "coins.symbol")
+    .select("binance_trading_pairs.symbol")
+    .orderBy("rank")
+    .whereNotIn("baseAsset", [
+      "BUSD",
+      "DAI",
+      "FEI",
+      "FRAX",
+      "GUSD",
+      "LUSD",
+      "TUSD",
+      "USDC",
+      "USDN",
+      "USDP",
+      "USDT",
+      "UST",
+    ])
+    .limit(36);
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = 0; j < intervals.length; j++) {
+      const tableName = `binance_${coins[i].symbol.toLowerCase()}_${
+        intervals[j]
+      }`;
+      const data = await db
+        .withSchema("ohlcvs")
+        .from(tableName)
+        // .select("*")
+        .where("time", ">=", "2021-06-01")
+        .del();
+      console.log(data);
+    }
+  }
+  console.log(coins);
+};
--- src/index.js
+++ src/index.js
@@ -2,29 +2,30 @@ import schedule from "node-schedule";
 import createOHLCVsFrom1m from "./createOHLCVsFrom1m";
 
 import db from "./database";
+import fixDatabase from "./database/fixDatabase";
 import downloadTokenLogos from "./downloadTokenLogo";
 import setTokenColors from "./setTokenColors";
 import updateBinanceOHLCVs from "./updateBinanceOHLCVs";
 import updateCoinmarketSymbols from "./updateCoinmarketSymbols";
 import updateExchangeSymbols from "./updateExchangeSymbols";
-
 (async () => {
+  // await fixDatabase();
   // await updateCoinmarketSymbols();
   // await updateExchangeSymbols();
   // await createOHLCVsFrom1m();
   // await downloadTokenLogos();
   // await setTokenColors();
-  // while (true) {
-  //   await updateBinanceOHLCVs("1m");
-  // }
+  while (true) {
+    await updateBinanceOHLCVs("1m");
+  }
   // await updateBinanceOHLCVs("1m");
 
   console.log("done");
 })();
 
-schedule.scheduleJob("3 * * * * *", async () => {
-  await updateBinanceOHLCVs("1m");
-});
+// schedule.scheduleJob("3 * * * * *", async () => {
+//   await updateBinanceOHLCVs("1m");
+// });
 
 // schedule.scheduleJob("* * 0 * * *", async () => {
 //   await updateCoinmarketSymbols();
